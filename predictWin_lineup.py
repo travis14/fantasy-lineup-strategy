007450c24a1c1ed105f15b313d5c9cdb577b9e9d
travis14 <travis14@stanford.edu>
2015-12-30 00:02:08 -0800

    cleaned up code and added data

--- predictWin_lineup.py
+++ predictWin_lineup.py
@@ -5,6 +5,25 @@ import scrapeGameLogs
 from scrapeGameLogs import getPlayerStats
 from players import players
 import copy
+import operator
+
+Austin = ['John Wall', 'Jrue Holiday', 'Thaddeus Young', 'Tobias Harris', 'Tim Duncan', 'Evan Fournier', 'Kevin Love', 'DeMarcus Cousins', 'Rudy Gay', 'Eric Gordon', 'Marc Gasol', 'Jahlil Okafor', 'Avery Bradley']
+Austin2 = ['Damian Lillard', 'Jrue Holiday', 'Thaddeus Young', 'Giannis Antetokounmpo', 'Tim Duncan', 'Evan Fournier', 'Gordon Hayward', 'Andre Drummond', 'Al Horford', 'Eric Gordon', 'Marc Gasol', 'Jahlil Okafor', 'Avery Bradley']
+Wayland = ['Stephen Curry', 'Brandon Knight', 'Kentavious Caldwell-Pope', 'Zach Randolph', 'Serge Ibaka', 'Kemba Walker', 'Nikola Jokic', 'Kyrie Irving', 'Elfrid Payton', 'Monta Ellis', 'Blake Griffin', 'Mike Conley', 'Will Barton']
+Sam = ['Goran Dragic', 'Nicolas Batum', 'Robert Covington', 'Julius Randle', 'Greg Monroe', 'Andrew Wiggins', 'Kawhi Leonard', 'Russell Westbrook', 'Chris Bosh', 'George Hill', 'Carmelo Anthony', 'Kyle Korver', 'Kristaps Porzingis']
+Raymond = ['Rajon Rondo', 'Victor Oladipo', 'Kevin Durant', 'Paul George', 'Karl-Anthony Towns', 'Reggie Jackson', 'Kobe Bryant', 'Dwyane Wade', 'Jordan Clarkson', 'Nikola Vucevic', 'Danilo Gallinari', 'Wesley Matthews', 'Harrison Barnes']
+Jeremy = ['Chris Paul', 'Kyle Lowry', 'Jae Crowder', 'LaMarcus Aldridge', 'DeAndre Jordan', 'Ricky Rubio', 'Pau Gasol', 'Terrence Jones', 'Gorgui Dieng', 'Tony Parker', 'Deron Williams', 'Derrick Favors', 'Zaza Pachulia']
+Travis = ['Damian Lillard', 'Tyreke Evans', 'LeBron James', 'Al Horford', 'Andre Drummond', 'Gordon Hayward', 'Trevor Ariza', 'Draymond Green', 'Dirk Nowitzki', 'Marcin Gortat', 'Giannis Antetokounmpo', 'Omri Casspi', 'Kent Bazemore']
+Travis2 = ['John Wall', 'Tyreke Evans', 'LeBron James', 'Kevin Love', 'DeMarcus Cousins', 'Rudy Gay', 'Trevor Ariza', 'Draymond Green', 'Dirk Nowitzki', 'Marcin Gortat', 'Tobias Harris', 'Omri Casspi', 'Kent Bazemore']
+Gabe = ['C.J. McCollum', 'James Harden', 'Klay Thompson', 'Hassan Whiteside', 'Al Jefferson', 'Isaiah Thomas', 'Kenneth Faried', 'Nerlens Noel', 'Jarrett Jack', 'J.J. Redick', 'Dwight Howard', 'Arron Afflalo']
+Hashmi = ['Michael Carter-Williams', 'Jimmy Butler', 'Ryan Anderson', 'Anthony Davis', 'Brook Lopez', 'Jeff Teague', 'Paul Millsap', 'Khris Middleton', 'Bradley Beal', 'David Lee', 'Jonas Valanciunas', 'DeMar DeRozan', 'Enes Kanter']
+allPlayers = [Austin2, Wayland, Sam, Raymond, Jeremy, Travis2]
+allPlayerNames = ['Austin', 'Wayland', 'Sam', 'Raymond', 'Jeremy', 'Travis']
+usedAgents = Austin + Wayland + Sam + Raymond + Jeremy + Travis + Gabe + Hashmi
+freeAgents = [agent for agent in players if agent not in usedAgents]
+playerNamesPointguards = {}
+for index, player in enumerate(allPlayers):
+	playerNamesPointguards[player[0]] = allPlayerNames[index]
 
 cache = {}
 
@@ -87,9 +106,13 @@ def probWin(myPlayers, oppPlayers, numDaysPerPlayer=3):
 			probWinningCategory[category] = 1.0 - scipy.stats.norm(differenceMean, differenceStd).cdf(0)
 		elif plusOrMinus == '-':
 			probWinningCategory[category] = scipy.stats.norm(differenceMean, differenceStd).cdf(0)
-		print 'Probability of winning %s: %f' % (category, probWinningCategory[category])
+		#print 'Probability of winning %s: %f' % (category, probWinningCategory[category])
 	#print 'myPlayerCategories: ',myPlayerCategories
 	#print 'oppPlayerCategories: ', oppPlayerCategories
+	netDifference = {}
+	for category in myPlayerCategories:
+		netDifference[category] = myPlayerCategories[category][0] - oppPlayerCategories[category][0]
+	#print 'Net difference: ', netDifference
 
 	categories = probWinningCategory.keys()
 	numCategories = float(len(categories))
@@ -109,39 +132,23 @@ def probWin(myPlayers, oppPlayers, numDaysPerPlayer=3):
 	#print 'Probability of winning more categories overall: ', probWinning
 	return probWinning
 
-Austin = ['John Wall', 'Jrue Holiday', 'Thaddeus Young', 'Tobias Harris', 'Tim Duncan', 'Evan Fournier', 'Kevin Love', 'DeMarcus Cousins', 'Rudy Gay', 'Eric Gordon', 'Marc Gasol', 'Jahlil Okafor', 'Avery Bradley']
-Wayland = ['Stephen Curry', 'Brandon Knight', 'Kentavious Caldwell-Pope', 'Zach Randolph', 'Serge Ibaka', 'Kemba Walker', 'Nikola Jokic', 'Kyrie Irving', 'Elfrid Payton', 'Mason Plumlee', 'Blake Griffin', 'Mike Conley', 'Will Barton']
-Sam = ['Goran Dragic', 'Nicolas Batum', 'Robert Covington', 'Julius Randle', 'Greg Monroe', 'Andrew Wiggins', 'Kawhi Leonard', 'Russell Westbrook', 'Chris Bosh', 'George Hill', 'Carmelo Anthony', 'Kyle Korver', 'Kristaps Porzingis']
-Raymond = ['Rajon Rondo', 'Victor Oladipo', 'Kevin Durant', 'Paul George', 'Karl-Anthony Towns', 'Reggie Jackson', 'Kobe Bryant', 'Dwyane Wade', 'Jordan Clarkson', 'Nikola Vucevic', 'Danilo Gallinari', 'Wesley Matthews', 'Harrison Barnes']
-Jeremy = ['Chris Paul', 'Kyle Lowry', 'Jae Crowder', 'LaMarcus Aldridge', 'DeAndre Jordan', 'Ricky Rubio', 'Pau Gasol', 'Terrence Jones', 'Gorgui Dieng', 'Tony Parker', 'Deron Williams', 'Derrick Favors', 'Zaza Pachulia']
-Travis = ['Damian Lillard', 'Tyreke Evans', 'LeBron James', 'Al Horford', 'Andre Drummond', 'Gordon Hayward', 'Trevor Ariza', 'Draymond Green', 'Dirk Nowitzki', 'Marcin Gortat', 'Giannis Antetokounmpo', 'Omri Casspi', 'Kent Bazemore']
-Gabe = ['C.J. McCollum', 'James Harden', 'Klay Thompson', 'Hassan Whiteside', 'Al Jefferson', 'Isaiah Thomas', 'Kenneth Faried', 'Nerlens Noel', 'Jarrett Jack', 'J.J. Redick', 'Dwight Howard', 'Arron Afflalo']
-Hashmi = ['Michael Carter-Williams', 'Jimmy Butler', 'Ryan Anderson', 'Anthony Davis', 'Brook Lopez', 'Jeff Teague', 'Paul Millsap', 'Khris Middleton', 'Bradley Beal', 'David Lee', 'Jonas Valanciunas', 'DeMar DeRozan', 'Enes Kanter']
-
-allPlayers = [Austin, Wayland, Sam, Raymond, Jeremy, Travis]
-allPlayerNames = ['Austin', 'Wayland', 'Sam', 'Raymond', 'Jeremy', 'Travis']
-usedAgents = Austin + Wayland + Sam + Raymond + Jeremy + Travis + Gabe + Hashmi
-freeAgents = [agent for agent in players if agent not in usedAgents]
-playerNamesPointguards = {}
-for index, player in enumerate(allPlayers):
-	playerNamesPointguards[player[0]] = allPlayerNames[index]
-
-
+# Function: probWinningAgainstOpponents
+# -------------------------
+# Determines the average probability that the playerLineup will win against all opponents by calling probWin for all other players and averaging
 def probWinningAgainstOpponents(player, playerLineup):
 	#Make opponents the list of everyone except yourself, i.e. for me it would be Austin, Wayland, Sam, Raymond, Jeremy
 	opponents = copy.deepcopy(allPlayers)
 	opponents.remove(player)
-	print playerNamesPointguards[player[0]] + ': '
+	#print playerNamesPointguards[player[0]] + ': '
 	#Make this list the same thing as opponents except with quotation marks around the names
 	#opponentNames = [x for x in allPlayerNames if x != playerNamesPointguards[player[0]]]
 	averageProbWin = 0
 	for index, opponent in enumerate(opponents):
-		#print
 		probabilityWin = probWin(playerLineup, opponent)
 		#print 'vs ' + opponent[0] + ': '
 		averageProbWin += probabilityWin
 	averageProbWin = averageProbWin/len(opponents)
-	print 'Average probability of winning: ' + str(averageProbWin)
+	#print 'Average probability of winning: ' + str(averageProbWin)
 	return averageProbWin
 
 # Function: evaluateTrade
@@ -149,7 +156,7 @@ def probWinningAgainstOpponents(player, playerLineup):
 # Evaluates the effect that a trade involving player1PlayersToTrade and player2PlayersToTrade will have on both players
 def evaluateTrade(player1, player2, player1PlayersToTrade, player2PlayersToTrade):
 	if not set(player1PlayersToTrade).issubset(set(player1)) or not set(player2PlayersToTrade).issubset(set(player2)):
-		print 'Players specified in the trade are not owned by the players'
+		#print 'Players specified in the trade are not owned by the players'
 		return 
 
 	player1Opponents = copy.deepcopy(allPlayers)
@@ -158,32 +165,43 @@ def evaluateTrade(player1, player2, player1PlayersToTrade, player2PlayersToTrade
 	player2Opponents = copy.deepcopy(allPlayers)
 	player2Opponents.remove(player2)
 
+	'''
 	print '#### Evaluating ' + playerNamesPointguards[player1[0]] + '\'s and ' + playerNamesPointguards[player2[0]] + '\'s trade of ' + str(player1PlayersToTrade) + ' for ' + str(player2PlayersToTrade) + ' ####'
 	print
 	print '---For ' + playerNamesPointguards[player1[0]] + ' pre-trade---'
+	'''
 	averageProbWin = 0
 	for index, opponent in enumerate(player1Opponents):
 		probWinning = probWin(player1, opponent)
+		'''
 		print 'Probability of winning vs ' + playerNamesPointguards[opponent[0]] + ': ' + str(probWinning)
+		'''
 		averageProbWin += probWinning
+	'''
 	print
+	'''
 	player1ProbWinning_pretrade = averageProbWin/len(player1Opponents)
+	'''
 	print 'Average pre-trade probability of winning: ', player1ProbWinning_pretrade
 
 	print
 	print '---For ' + playerNamesPointguards[player2[0]] + ' pre-trade---'
+	'''
 	averageProbWin = 0
 	for index, opponent in enumerate(player2Opponents):
 		probWinning = probWin(player2, opponent)
-		print 'Probability of winning vs ' + playerNamesPointguards[opponent[0]] + ': ' + str(probWinning)
+		#print 'Probability of winning vs ' + playerNamesPointguards[opponent[0]] + ': ' + str(probWinning)
 		averageProbWin += probWinning
+	'''
 	print
+	'''
 	player2ProbWinning_pretrade = averageProbWin/len(player1Opponents)
+	'''
 	print 'Average pre-trade probability of winning: ', player2ProbWinning_pretrade
-
+	'''
 	player1PostTrade = [x for x in player1 if x not in player1PlayersToTrade]
 	player1PostTrade += player2PlayersToTrade
-	
+
 	player2PostTrade = [x for x in player2 if x not in player2PlayersToTrade]
 	player2PostTrade += player1PlayersToTrade
 
@@ -193,8 +211,10 @@ def evaluateTrade(player1, player2, player1PlayersToTrade, player2PlayersToTrade
 	player2Opponents.remove(player1)
 	player2Opponents.append(player1PostTrade)
 
+	'''
 	print
 	print '---For ' + playerNamesPointguards[player1[0]] + ' post-trade---'
+	'''
 	averageProbWin = 0
 	for index, opponent in enumerate(player1Opponents):
 		if index == len(player1Opponents) - 1:
@@ -202,14 +222,18 @@ def evaluateTrade(player1, player2, player1PlayersToTrade, player2PlayersToTrade
 		else:
 			name = playerNamesPointguards[opponent[0]]
 		probWinning = probWin(player1PostTrade, opponent)
+		'''
 		print 'Probability of winning vs ' + name + ': ' + str(probWinning)
+		'''
 		averageProbWin += probWinning
-	print
+	#print
 	player1ProbWinning_posttrade = averageProbWin/len(player1Opponents)
+	'''
 	print 'Average post-trade probability of winning: ', player1ProbWinning_posttrade
 
 	print
 	print '---For ' + playerNamesPointguards[player2[0]] + ' post-trade---'
+	'''
 	averageProbWin = 0
 	for index, opponent in enumerate(player2Opponents):
 		if index == len(player2Opponents) - 1:
@@ -217,29 +241,45 @@ def evaluateTrade(player1, player2, player1PlayersToTrade, player2PlayersToTrade
 		else:
 			name = playerNamesPointguards[opponent[0]]
 		probWinning = probWin(player2PostTrade, opponent)
-		print 'Probability of winning vs ' + name + ': ' + str(probWinning)
+		#print 'Probability of winning vs ' + name + ': ' + str(probWinning)
 		averageProbWin += probWinning
-	print
+	#print
 	player2ProbWinning_posttrade = averageProbWin/len(player1Opponents)
-	print 'Average post-trade probability of winning: ', player2ProbWinning_posttrade
+	#print 'Average post-trade probability of winning: ', player2ProbWinning_posttrade
 
+	'''
 	print
 	print 'Trade increased ' + str(playerNamesPointguards[player1[0]]) + '\'s average probability of winning by ' + str(player1ProbWinning_posttrade - player1ProbWinning_pretrade) + ' from ' + str(player1ProbWinning_pretrade) + ' to ' + str(player1ProbWinning_posttrade)
 	print 'Trade increased ' + str(playerNamesPointguards[player2[0]]) + '\'s average probability of winning by ' + str(player2ProbWinning_posttrade - player2ProbWinning_pretrade) + ' from ' + str(player2ProbWinning_pretrade) + ' to ' + str(player2ProbWinning_posttrade)
+	'''
 	return (player1ProbWinning_posttrade - player1ProbWinning_pretrade, player2ProbWinning_posttrade - player2ProbWinning_pretrade)
 
+# Function: findMutuallyBeneficialTrade
+# -------------------------
+# Calls evaluateTrade on all possible one-player and two-player trades to see which trades are net beneficial to both sides
 def findMutuallyBeneficialTrade(player1, player2):
 	mutuallyBeneficialTrades = {}
 	#one player trades 
-	trades = list(itertools.product(player1, player2))
-	for trade in trades:
-		print trade
+	oneplayer_trades = list(itertools.product(player1, player2))
+	for trade in oneplayer_trades:
+		#print trade
 		tradeImpact = evaluateTrade(player1, player2, [trade[0]], [trade[1]])
 		if tradeImpact[0] > 0 and tradeImpact[1] > 0:
 			mutuallyBeneficialTrades[trade] = tradeImpact
+
+	twoplayer_trades = list(itertools.product(itertools.combinations(player1, 2), itertools.combinations(player2, 2)))
+	for trade in twoplayer_trades:
+		#print trade
+		tradeImpact = evaluateTrade(player1, player2, [x for x in trade[0]], [x for x in trade[1]])
+		if tradeImpact[0] > 0 and tradeImpact[1] > 0:
+			mutuallyBeneficialTrades[trade] = tradeImpact
+
 	print mutuallyBeneficialTrades
 	return mutuallyBeneficialTrades
 
+# Function: findGoodFreeAgents
+# -------------------------
+# Goes through all the players that are not currently on a team and returns the agents that would be net beneficial to a given player
 def findGoodFreeAgents(player1):
 	goodPickups = {}
 	for player in player1:
@@ -252,15 +292,15 @@ def findGoodFreeAgents(player1):
 			if probNew > probOld:
 				goodPickup = (player, fa)
 				goodPickups[goodPickup] = probNew - probOld
-	print goodPickups
-	sorted_pickups = sorted(goodPickups.items(), key=operator.itemgetter(1))
-	return goodPickups
+	sorted_pickups = sorted(goodPickups.items(), key=operator.itemgetter(1), reverse=True)
+	print sorted_pickups
+	return sorted_pickups
 
-print findMutuallyBeneficialTrade(Travis, Austin)
-
+#example commands
 '''
-print probWin(['Damian Lillard', 'Al Horford', 'Gordon Hayward', 'Andre Drummond', 'Giannis Antetokounmpo'], ['John Wall', 'Kevin Love', 'Rudy Gay', 'DeMarcus Cousins', 'Tobias Harris'])
+findMutuallyBeneficialTrade(Travis2, Austin2)
+evaluateTrade(Travis2, Wayland, ['Dirk Nowitzki'], ['Serge Ibaka'])
+print probWin(Travis2, Austin2)
 evaluateTrade(Travis, Austin, ['Damian Lillard'], ['John Wall'])
 findGoodFreeAgents(Travis)
 '''
-
--- scrapeGameLogs.py
+++ scrapeGameLogs.py
@@ -23,73 +23,76 @@ def getGameLogs(name):
 	nameWithHyphen = "-".join(name.lower().split())
 	try:
 		profile_link = next(link for link in player_profile_links if nameWithHyphen in link)
+		game_log_link = profile_link.split('player', 1)[0] + 'player/gamelog' + profile_link.split('player', 1)[1]
+		try:
+			game_log_soup = BeautifulSoup(urlopen(game_log_link), 'lxml')
+			table = game_log_soup.find_all('table')[1]
+			rows = table.find_all('tr')
+			categories = [row.text for row in rows[1].find_all('td')]
+			logs = []
+			for row in rows:
+				if 'PRESEASON' in row.find('td').text:
+					break
+				#relevant rows have a class tag of ['oddrow', 'team-46-28'], for example
+				if len(row['class']) == 2: 
+					log = {}
+					for index, stat in enumerate(row.find_all('td')):
+						category = categories[index]
+						log[category] = stat.text
+					logs.append(log)
+			return logs
+		except:
+			print 'game log with error: ', game_log_link
+			#print categories
 	except:
 		print 'name with error: ', name
-	game_log_link = profile_link.split('player', 1)[0] + 'player/gamelog' + profile_link.split('player', 1)[1]
-	try:
-		game_log_soup = BeautifulSoup(urlopen(game_log_link), 'lxml')
-		table = game_log_soup.find_all('table')[1]
-		rows = table.find_all('tr')
-		categories = [row.text for row in rows[1].find_all('td')]
-	except:
-		print 'game log with error: ', game_log_link
-		#print categories
-	logs = []
-	for row in rows:
-		if 'PRESEASON' in row.find('td').text:
-			break
-		#relevant rows have a class tag of ['oddrow', 'team-46-28'], for example
-		if len(row['class']) == 2: 
-			log = {}
-			for index, stat in enumerate(row.find_all('td')):
-				category = categories[index]
-				log[category] = stat.text
-			logs.append(log)
-	return logs
 
 # Function: getPlayerStats
 # -------------------------
 # From the game logs of a given player, returns a dictionary of category to (mean, standard deviation) tuples
 def getPlayerStats(name):
 	gameLogs = getGameLogs(name)
-	numGames = 0
-	formattedGameLogs = []
-	for log in gameLogs:
-		if int(log['MIN']) > 0:
-			formattedLog = {}
-			formattedLog['FGM-FGA'] = (float(log['FGM-FGA'].split('-')[0]), float(log['FGM-FGA'].split('-')[1]))
-			formattedLog['FTM-FTA'] = (float(log['FTM-FTA'].split('-')[0]), float(log['FTM-FTA'].split('-')[1])) 
-			formattedLog['3PM'] = float(log['3PM-3PA'].split('-')[0])
-			formattedLog['REB'] = float(log['REB'])
-			formattedLog['AST'] = float(log['AST'])
-			formattedLog['STL'] = float(log['STL'])
-			formattedLog['BLK'] = float(log['BLK'])
-			formattedLog['TO'] = float(log['TO'])
-			formattedLog['PTS'] = float(log['PTS'])
-			formattedGameLogs.append(formattedLog)
-			numGames += 1
-	if len(formattedGameLogs) < 2:
+	if gameLogs:
+		numGames = 0
+		formattedGameLogs = []
+		for log in gameLogs:
+			if int(log['MIN']) > 0:
+				formattedLog = {}
+				formattedLog['FGM-FGA'] = (float(log['FGM-FGA'].split('-')[0]), float(log['FGM-FGA'].split('-')[1]))
+				formattedLog['FTM-FTA'] = (float(log['FTM-FTA'].split('-')[0]), float(log['FTM-FTA'].split('-')[1])) 
+				formattedLog['3PM'] = float(log['3PM-3PA'].split('-')[0])
+				formattedLog['REB'] = float(log['REB'])
+				formattedLog['AST'] = float(log['AST'])
+				formattedLog['STL'] = float(log['STL'])
+				formattedLog['BLK'] = float(log['BLK'])
+				formattedLog['TO'] = float(log['TO'])
+				formattedLog['PTS'] = float(log['PTS'])
+				formattedGameLogs.append(formattedLog)
+				numGames += 1
+		if len(formattedGameLogs) < 2:
+			return {'FGM': (0.0, 0.0), 'FGA': (0.0, 0.0), 'FTM': (0.0, 0.0), 'FTA': (0.0, 0.0), '3PM': (0.0, 0.0), 'REB': (0.0, 0.0), 'AST': (0.0, 0.0), 'STL': (0.0, 0.0), 'BLK':(0.0, 0.0), 'TO': (0.0, 0.0), 'PTS': (0.0, 0.0)}
+		fgms = [log['FGM-FGA'][0] for log in formattedGameLogs]
+		fgas = [log['FGM-FGA'][1] for log in formattedGameLogs]
+		ftms = [log['FTM-FTA'][0] for log in formattedGameLogs]
+		ftas = [log['FTM-FTA'][1] for log in formattedGameLogs]
+		threepms = [log['3PM'] for log in formattedGameLogs]
+		rebs = [log['REB'] for log in formattedGameLogs]
+		asts = [log['AST'] for log in formattedGameLogs]
+		stls = [log['STL'] for log in formattedGameLogs]
+		blks = [log['BLK'] for log in formattedGameLogs]
+		tos = [log['TO'] for log in formattedGameLogs]
+		pts = [log['PTS'] for log in formattedGameLogs]
+		fgm_stats = (statistics.mean(fgms), statistics.stdev(fgms))
+		fga_stats = (statistics.mean(fgas), statistics.stdev(fgas))
+		ftm_stats = (statistics.mean(ftms), statistics.stdev(ftms))
+		fta_stats = (statistics.mean(ftas), statistics.stdev(ftas))
+		threepm_stats = (statistics.mean(threepms), statistics.stdev(threepms))
+		reb_stats = (statistics.mean(rebs), statistics.stdev(rebs))
+		ast_stats = (statistics.mean(asts), statistics.stdev(asts))
+		stl_stats = (statistics.mean(stls), statistics.stdev(stls))
+		blk_stats = (statistics.mean(blks), statistics.stdev(blks))
+		to_stats = (statistics.mean(tos), statistics.stdev(tos))
+		pts_stats = (statistics.mean(pts),  statistics.stdev(pts))
+		return {'FGM': fgm_stats, 'FGA': fga_stats, 'FTM': ftm_stats, 'FTA': fta_stats, '3PM': threepm_stats, 'REB': reb_stats, 'AST': ast_stats, 'STL': stl_stats, 'BLK':blk_stats, 'TO': to_stats, 'PTS': pts_stats}
+	else:
 		return {'FGM': (0.0, 0.0), 'FGA': (0.0, 0.0), 'FTM': (0.0, 0.0), 'FTA': (0.0, 0.0), '3PM': (0.0, 0.0), 'REB': (0.0, 0.0), 'AST': (0.0, 0.0), 'STL': (0.0, 0.0), 'BLK':(0.0, 0.0), 'TO': (0.0, 0.0), 'PTS': (0.0, 0.0)}
-	fgms = [log['FGM-FGA'][0] for log in formattedGameLogs]
-	fgas = [log['FGM-FGA'][1] for log in formattedGameLogs]
-	ftms = [log['FTM-FTA'][0] for log in formattedGameLogs]
-	ftas = [log['FTM-FTA'][1] for log in formattedGameLogs]
-	threepms = [log['3PM'] for log in formattedGameLogs]
-	rebs = [log['REB'] for log in formattedGameLogs]
-	asts = [log['AST'] for log in formattedGameLogs]
-	stls = [log['STL'] for log in formattedGameLogs]
-	blks = [log['BLK'] for log in formattedGameLogs]
-	tos = [log['TO'] for log in formattedGameLogs]
-	pts = [log['PTS'] for log in formattedGameLogs]
-	fgm_stats = (statistics.mean(fgms), statistics.stdev(fgms))
-	fga_stats = (statistics.mean(fgas), statistics.stdev(fgas))
-	ftm_stats = (statistics.mean(ftms), statistics.stdev(ftms))
-	fta_stats = (statistics.mean(ftas), statistics.stdev(ftas))
-	threepm_stats = (statistics.mean(threepms), statistics.stdev(threepms))
-	reb_stats = (statistics.mean(rebs), statistics.stdev(rebs))
-	ast_stats = (statistics.mean(asts), statistics.stdev(asts))
-	stl_stats = (statistics.mean(stls), statistics.stdev(stls))
-	blk_stats = (statistics.mean(blks), statistics.stdev(blks))
-	to_stats = (statistics.mean(tos), statistics.stdev(tos))
-	pts_stats = (statistics.mean(pts),  statistics.stdev(pts))
-	return {'FGM': fgm_stats, 'FGA': fga_stats, 'FTM': ftm_stats, 'FTA': fta_stats, '3PM': threepm_stats, 'REB': reb_stats, 'AST': ast_stats, 'STL': stl_stats, 'BLK':blk_stats, 'TO': to_stats, 'PTS': pts_stats}
